389b3f9bf59597d7a215a1256e095c4dba740771
Melchor Callos <148925008+melcal04@users.noreply.github.com>
2026-07-16 12:36:21 +0800

    Update: Authentication pages

--- frontend/src/layouts/AuthLayout.jsx
+++ frontend/src/layouts/AuthLayout.jsx
@@ -3,15 +3,12 @@ import Footer from "../components/Footer.jsx";
 
 export default function AuthLayout() {
   return (
-    <div id="layoutAuthentication" className="bg-body">
-      <div id="layoutAuthentication_content">
-        <main
-          className="d-flex align-items-center"
-          style={{ minHeight: "100%" }}
-        >
+    <div id="layoutAuthentication" className="bg-body auth-bg d-flex flex-column" style={{ minHeight: "100vh" }}>
+      <div id="layoutAuthentication_content" className="d-flex flex-column flex-grow-1">
+        <main className="d-flex align-items-center justify-content-center flex-grow-1">
           <div className="container p-5">
             <div className="row justify-content-center">
-              <div className="col-12 d-flex flex-column align-items-center">
+              <div className="col-12 d-flex flex-column align-items-center auth-animate">
                 <Outlet />
               </div>
             </div>
